94f9c8beb8174c11336f83017917a59ec7ef430b
epiclarkis <jlandoay@jlandoay-Macbook-Pro.local>
2023-10-30 09:59:36 -0400

    Updated flowmap

--- src/components/Flowmap.js
+++ src/components/Flowmap.js
@@ -4,6 +4,10 @@ const Flowmap = () => {
     return (
         <div className="flowmap">
             <h2>Chatbot Flowmap</h2>
+            <div className="legend">
+                <p>Bot Response &#128073; Answer Content in Ada</p>
+                <p>User Input &#128073; Answer Training in Ada</p>
+            </div>
             <div className="flowmap_container">
                 <img className="screeny" src={screeny} alt="flowmap" />
             </div>
